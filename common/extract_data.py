e40f9d34a1134b2234602824e2267fc4cc30f53b
Jacob Spoelstra <jaspoels@microsoft.com>
2019-11-12 16:02:02 -0800

    notebook and utility code for downloading data

--- common/extract_data.py
+++ common/extract_data.py
@@ -4,13 +4,18 @@ import sys
 import pandas as pd
 
 # This function unzips the GEFCom2014 data zip file and extracts the 'extended'
-# load forecasting competition data. Data is saved in energy.csv
+# load forecasting competition data. Data is saved in energy.csv.gz
+
+
 def extract_data(data_dir):
     GEFCom_dir = os.path.join(data_dir, 'GEFCom2014', 'GEFCom2014 Data')
 
     GEFCom_zipfile = os.path.join(data_dir, 'GEFCom2014.zip')
     if not os.path.exists(GEFCom_zipfile):
-        sys.exit("Download GEFCom2014.zip from https://www.dropbox.com/s/pqenrr2mcvl0hk9/GEFCom2014.zip?dl=0 and save it to the '{}' directory.".format(data_dir))
+        sys.exit(
+            "Download GEFCom2014.zip from " +
+            "https://www.dropbox.com/s/pqenrr2mcvl0hk9/GEFCom2014.zip?dl=0 " +
+            "and save it to the '{}' directory.".format(data_dir))
 
     # unzip root directory
     zip_ref = zipfile.ZipFile(GEFCom_zipfile, 'r')
@@ -18,20 +23,29 @@ def extract_data(data_dir):
     zip_ref.close()
 
     # extract the extended competition data
-    zip_ref = zipfile.ZipFile(os.path.join(GEFCom_dir, 'GEFCom2014-E_V2.zip'), 'r')
+    zip_ref = zipfile.ZipFile(
+        os.path.join(GEFCom_dir, 'GEFCom2014-E_V2.zip'),
+        'r')
     zip_ref.extractall(os.path.join(data_dir, 'GEFCom2014-E'))
     zip_ref.close()
 
     # load the data from Excel file
-    data = pd.read_excel(os.path.join(data_dir, 'GEFCom2014-E', 'GEFCom2014-E.xlsx'), parse_date='Date')
+    data = pd.read_excel(
+        os.path.join(data_dir, 'GEFCom2014-E', 'GEFCom2014-E.xlsx'),
+        parse_date='Date')
 
     # create timestamp variable from Date and Hour
-    data['timestamp'] = data['Date'].add(pd.to_timedelta(data.Hour - 1, unit='h'))
+    data['timestamp'] = data['Date'].add(
+        pd.to_timedelta(data.Hour - 1, unit='h'))
     data = data[['timestamp', 'load', 'T']]
-    data = data.rename(columns={'T':'temp'})
+    data = data.rename(columns={'T': 'temp'})
 
     # remove time period with no load data
     data = data[data.timestamp >= '2012-01-01']
 
     # save to csv
-    data.to_csv(os.path.join(data_dir, 'energy.csv'), index=False)
+    pd.DataFrame.to_csv()
+    data.to_csv(
+        os.path.join(data_dir, 'energy.csv.gz'),
+        compression="gzip",
+        index=False)
