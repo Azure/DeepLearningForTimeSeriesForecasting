7617d1d911e19b92d61dc720fa33af63ab90f69d
Jacob Spoelstra <jaspoels@microsoft.com>
2019-12-11 14:29:56 -0500

    Fixed bugs in hyperparam

--- common/utils.py
+++ common/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from collections import UserDict
+import os
 
 
 def mape(predictions, actuals):
@@ -22,6 +23,27 @@ def create_evaluation_df(predictions, test_inputs, H, scaler):
         eval_df[["prediction", "actual"]]
     )
     return eval_df
+
+
+def load_data(data_dir):
+    """Load the GEFCom 2014 energy load data"""
+
+    energy = pd.read_csv(
+        os.path.join(data_dir, 'energy.csv.gz'), parse_dates=['timestamp']
+    )
+
+    # Reindex the dataframe such that the dataframe has a record for
+    # every time point between the minimum and maximum timestamp in
+    # the time series. This helps to identify missing time periods in
+    # the data (there are none in this dataset).
+
+    energy.index = energy['timestamp']
+    energy = energy.reindex(pd.date_range(min(energy['timestamp']),
+                                          max(energy['timestamp']),
+                                          freq='H'))
+    energy = energy.drop('timestamp', axis=1)
+
+    return energy
 
 
 class TimeSeriesTensor(UserDict):
